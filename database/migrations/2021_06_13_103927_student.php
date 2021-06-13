ab2345be77bb70c70c353f4215fdebb1afe1b6af
Tala-Hodeib <talahodeib@gmail.com>
2021-06-13 16:29:38 +0300

    first commit

---
+++ database/migrations/2021_06_13_103927_student.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Student extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->id();
+            $table->string('student_code');
+            $table->string('first_name');
+            $table->string('father_name');
+            $table->string('last_name');
+            $table->numeric('mobile_number');
+            $table->string('email');
+            $table->string('password');
+            $table->date('DOB');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student');
+    }
+}
